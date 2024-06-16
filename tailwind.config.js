624c02b788f6f3b3b163922ce603f92dea0d38a2
Muhammad Zawar <mzawar189@gmail.com>
2024-06-16 11:13:24 +0500

    First Bytewise Fellowship Project-1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["*"],
+
+  theme: {
+    colors: {
+      brown: "hsl(14, 45%, 36%)",
+      purple: "hsl(332, 51%, 32%)",
+      Pink: "hsl(330, 100%, 96%)",
+      black: "hsl(0, 0%, 0%)",
+      charCoal: "hsl(24, 5%, 18%)",
+      gray: "hsl(30, 18%, 87%)",
+      white: "hsl(0, 0%, 100%)",
+      BackColor: "hsl(20, 60%, 90%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
